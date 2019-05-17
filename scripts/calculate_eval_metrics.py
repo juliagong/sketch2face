0aebacfc0903b19187af0d72c85e1dcf5de37a0d
Matt Mistele <mmistele@stanford.edu>
2019-05-17 16:19:50 -0700

    SSIM computation

--- scripts/calculate_eval_metrics.py
+++ scripts/calculate_eval_metrics.py
@@ -16,6 +16,7 @@ import re
 import cv2
 import numpy as np
 from docopt import docopt
+from skimage.measure import compare_ssim as ssim
 
 def make_image_path_pairs(dir):
     image_pairs = []
@@ -69,14 +70,18 @@ def main():
 
     l1_distances = []
     l2_distances = []
+    ssims = []
     for fake, real in image_pairs:
         l1_distances.append(l1_distance(fake, real))
         l2_distances.append(l2_distance(fake, real))
+        ssims.append(ssim(fake, real, multichannel=True))
 
     avg_l1_dist = sum(l1_distances)/len(l1_distances)
     avg_l2_dist = sum(l2_distances)/len(l2_distances)
+    avg_ssim = sum(ssims)/len(ssims)
     print("Average L1 dist between the {} image pairs: {}".format(len(l1_distances), avg_l1_dist))
     print("Average L2 dist between the {} image pairs: {}".format(len(l2_distances), avg_l2_dist))
+    print("Average SSIM between the {} image pairs: {}".format(len(ssims), avg_ssim))
 
 if __name__ == "__main__":
 	main()
