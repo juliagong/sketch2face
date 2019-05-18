c033a513c20f58acd15782e8f7b2d65b277b2237
Matt Mistele <mmistele@stanford.edu>
2019-05-17 18:49:38 -0700

    Switching eval metric script + notebook to use RMSE
    Also adding inception score possibility, gated by --include_inception flag, which doesn't run to completion on my computer yet.

--- scripts/calculate_eval_metrics.py
+++ scripts/calculate_eval_metrics.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+# Credit to https://www.pyimagesearch.com/2014/09/15/python-compare-two-images/ for ideas
 """
 NMT Transformer experiment
 
@@ -9,7 +10,7 @@ Usage:
 Options:
     -h --help                               show this screen.
     --images_path=<string>                  path to images to compare                         
-
+    --include_inception                     whether to calculate inception score (long & buggy right now)
 """
 import os
 import re
@@ -52,15 +53,15 @@ def make_image_path_pairs(dir):
                 real_paths[key] = path
     return image_pairs
 
+def MAE(imageA, imageB): # same as torch.nn.L1Loss, which is put out
+    abs_error = np.sum(abs(imageA.astype("float") - imageB.astype("float")))
+    mean_abs_error = abs_error / float(imageA.shape[0] * imageB.shape[1] * imageB.shape[2])
+    return mean_abs_error
 
-
-def l1_distance(imageA, imageB):
-    sq_error = np.sum(abs(imageA.astype("float") - imageB.astype("float")))
-    return sq_error / float(imageA.shape[0] * imageB.shape[1] * imageB.shape[2])
-
-def l2_distance(imageA, imageB):
+def RMSE(imageA, imageB): # Root-mean-squared error, equivalent of L2
     sq_error = np.sum((imageA.astype("float") - imageB.astype("float"))**2)
-    return sq_error / float(imageA.shape[0] * imageB.shape[1] * imageB.shape[2])
+    mse = sq_error / float(imageA.shape[0] * imageB.shape[1] * imageB.shape[2])
+    return np.sqrt(mse)
 
 def main():
     args = docopt(__doc__)
@@ -72,16 +73,24 @@ def main():
     l2_distances = []
     ssims = []
     for fake, real in image_pairs:
-        l1_distances.append(l1_distance(fake, real))
-        l2_distances.append(l2_distance(fake, real))
+        l1_distances.append(MAE(fake, real))
+        l2_distances.append(RMSE(fake, real))
         ssims.append(ssim(fake, real, multichannel=True))
 
     avg_l1_dist = sum(l1_distances)/len(l1_distances)
     avg_l2_dist = sum(l2_distances)/len(l2_distances)
     avg_ssim = sum(ssims)/len(ssims)
-    print("Average L1 dist between the {} image pairs: {}".format(len(l1_distances), avg_l1_dist))
-    print("Average L2 dist between the {} image pairs: {}".format(len(l2_distances), avg_l2_dist))
+    
+    print("Average mean average error (l1 loss) between the {} image pairs: {}".format(len(l1_distances), avg_l1_dist))
+    print("Average root mean squared error (l2 loss) between the {} image pairs: {}".format(len(l2_distances), avg_l2_dist))
     print("Average SSIM between the {} image pairs: {}".format(len(ssims), avg_ssim))
+    
+    if (args['--include_inception']):
+        from inception_distance import get_fid
+        fakes = np.array([fake for fake, _ in image_pairs]).transpose((0, 3, 1, 2))
+        reals = np.array([real for _, real in image_pairs]).transpose((0, 3, 1, 2))
+        avg_inception = get_fid(fakes, reals)
+        print("Frechet inception distance between the distributions: {}".format(avg_inception))
 
 if __name__ == "__main__":
 	main()
